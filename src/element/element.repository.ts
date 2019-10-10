a94cad7a752fef0a75caafd1b0785c1433b31bd0
Unknown <MATI50013@wp.pl>
2019-10-10 12:59:39 +0200

    Repository for Elements Created

--- src/element/element.repository.ts
+++ src/element/element.repository.ts
@@ -1,5 +1,8 @@
 import { Element } from './entity/element.entity';
 import { EntityRepository, Repository } from 'typeorm';
+import ElementDTO from './Element.DTO';
 
 @EntityRepository(Element)
-export class ElementRepository extends Repository<Element> {}
+export class ElementRepository extends Repository<Element> {
+  public createElement(ElementDTO: ElementDTO) {}
+}
