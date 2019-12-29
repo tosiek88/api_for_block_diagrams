3e6bd8c682294f960a65eeb0f38ad88893f65f7a
Tocha Mateusz <tocha.mateusz@gmail.com>
2019-12-29 14:52:04 +0100

    E2E creating entity, against testing database

--- src/element/element.service.ts
+++ src/element/element.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Element } from './entity/element.entity';
