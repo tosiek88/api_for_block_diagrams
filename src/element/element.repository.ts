94ce5252966347019d4cfddabf6ce3641fdde3d3
Tocha Mateusz <tocha.mateusz@gmail.com>
2020-02-04 22:30:03 +0100

    Indent fix, logs added for debugging.

--- src/element/element.repository.ts
+++ src/element/element.repository.ts
@@ -6,29 +6,30 @@ import ElementDTO from './Element.DTO';
 
 @EntityRepository(Element)
 export default class ElementRepo extends Repository<Element> {
-  constructor() {
-    super();
-  }
-  // tslint:disable-next-line: no-shadowed-variable
-  public async createElement(elementDTO: ElementDTO): Promise<ElementDTO> {
-    const elementEntity = new Element();
-    elementEntity.name = elementDTO.name;
-    elementEntity.connections = [];
-
-    if (elementDTO.connections !== undefined) {
-      elementDTO.connections.forEach(it => {
-        const tempConn: Connection = new Connection();
-        tempConn.id = it.id;
-        tempConn.label = it.label;
-        tempConn.elements = [];
-
-        elementEntity.connections.push(tempConn);
-      });
-    } else {
-      elementDTO.connections = [];
+    constructor() {
+        super();
     }
+    // tslint:disable-next-line: no-shadowed-variable
+    public async createElement(elementDTO: ElementDTO): Promise<ElementDTO> {
+        const elementEntity = new Element();
+        elementEntity.name = elementDTO.name;
+        elementEntity.connections = [];
+        if (elementDTO.connections !== undefined) {
+            elementDTO.connections.forEach(it => {
+                const tempConn: Connection = new Connection();
+                tempConn.id = it.id;
+                tempConn.label = it.label;
+                tempConn.elements = [];
 
-    const result = await this.save([elementEntity]);
-    return elementDTO;
-  }
+                elementEntity.connections.push(tempConn);
+            });
+        } else {
+            elementDTO.connections = [];
+
+        }
+
+        const dbResult = await this.save([elementEntity]);
+        Logger.log(dbResult);
+        return elementDTO;
+    }
 }
