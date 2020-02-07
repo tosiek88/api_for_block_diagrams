af0de5faf11576f75b349e95ce8692189895f0d0
Tocha Mateusz <tocha.mateusz@gmail.com>
2020-02-07 18:49:10 +0100

    Organize Imports

--- src/element/e2e/element.module.e2e-test.ts
+++ src/element/e2e/element.module.e2e-test.ts
@@ -1,22 +1,23 @@
-import ElementRepo from './../element.repository';
-import { Element } from './../entity/element.entity';
-import ElementDTO from './../Element.DTO';
-import { ElementService } from './../element.service';
-import { ElementController } from './../element.controller';
-import { Connection } from './../../connection/entity/connection.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-require('custom-env').env('test');
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, Logger } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { classToPlain } from 'class-transformer';
 import * as request from 'supertest';
+import { Connection } from './../../connection/entity/connection.entity';
 import {
   cleanDatabase,
   dbConnectionOptions,
   getSampleData,
 } from './../../utils/dbHelper';
+import { ElementController } from './../element.controller';
+import ElementDTO from './../Element.DTO';
+import ElementRepo from './../element.repository';
+import { ElementService } from './../element.service';
+import { Element } from './../entity/element.entity';
 import * as sampleData from './test_database.json';
-import { async } from 'rxjs/internal/scheduler/async';
+
+require('custom-env').env('test');
+
 const DATABASE = dbConnectionOptions(process.env.ORM_CONFIG_NAME);
 
 describe('Element', () => {
@@ -25,7 +26,7 @@ describe('Element', () => {
   let elementService: ElementService;
   let elementRepository: ElementRepo;
 
-  //TODO metadata can be stored somewhere else
+  // TODO metadata can be stored somewhere else
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
