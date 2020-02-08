a639b0a065e07d1f8807aa0fd0d9cd32ac290e66
Tocha Mateusz <tocha.mateusz@gmail.com>
2020-02-08 16:11:22 +0100

    Automapper implemented

--- src/element/element.repository.ts
+++ src/element/element.repository.ts
@@ -1,34 +1,34 @@
 import { Logger } from '@nestjs/common';
-import { Connection } from '../connection/entity/connection.entity';
-import { Element } from './entity/element.entity';
+import 'automapper-ts';
 import { EntityRepository, Repository } from 'typeorm';
 import ElementDTO from './Element.DTO';
+import { Element } from './entity/element.entity';
 
 @EntityRepository(Element)
 export default class ElementRepo extends Repository<Element> {
-    constructor() {
-        super();
-    }
-    // tslint:disable-next-line: no-shadowed-variable
-    public async createElement(elementDTO: ElementDTO): Promise<ElementDTO> {
-        const elementEntity = new Element();
-        elementEntity.name = elementDTO.name;
-        elementEntity.connections = [];
-        if (elementDTO.connections !== undefined) {
-            elementDTO.connections.forEach(it => {
-                const tempConn: Connection = new Connection();
-                tempConn.id = it.id;
-                tempConn.label = it.label;
-                tempConn.elements = [];
+  constructor() {
+    super();
+  }
+  public async createElement(elementDTO: ElementDTO): Promise<ElementDTO> {
+    let elementEntity = new Element();
+    automapper
+      .createMap('ConnectionDTO', 'Connection')
+      .forAllMembers((dest: any, destProp: string, value: any): void => {
+        dest[destProp] = value;
+        Logger.log(`${dest[destProp]}`, `AutoMapper`);
+      });
 
-                elementEntity.connections.push(tempConn);
-            });
-        } else {
-            elementDTO.connections = [];
+    automapper
+      .createMap('ElementDTO', 'Element')
+      .forAllMembers((dest: any, destProp: string, value: any): void => {
+        dest[destProp] = value;
+      });
+    // .forMember('connections', opt => {
+    //   Logger.log(opt);
+    // });
 
-        }
-
-        await this.save([elementEntity]);
-        return elementDTO;
-    }
+    elementEntity = automapper.map('ElementDTO', 'Element', elementDTO);
+    await this.save([elementEntity]);
+    return elementDTO;
+  }
 }
